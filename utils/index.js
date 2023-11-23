4d2899461ffc2b438384c34a048bd1dd0daa5627
aufall02 <aufaluzumaki@gmail.com>
2023-11-23 22:58:41 +0700

    chore : update utils

--- utils/index.js
+++ utils/index.js
@@ -1,9 +1,15 @@
 const { hashPassword, comparePassword } = require("./bcrypt");
-const format = require("./formatTime")
+const format = require("./formatTime");
+const { generateToken, verifyToken } = require("./jsonwebtoken");
+
+
+
 
 module.exports = {
     hashPassword,
     comparePassword,
-    format
+    format,
+    generateToken,
+    verifyToken
 }
 
